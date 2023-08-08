9213869ba7459db21eae71d7fcbe09c6bcc78c92
efff <127970886+ifkan15@users.noreply.github.com>
2023-08-08 15:59:53 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// registor plugins
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+    wrapper: '.wrapper',
+    content: '.content',
+    smooth: 1.5,
+    effects: true
+})
+
+gsap.fromTo('.hero-section', { opacity: 1 }, {
+    opacity: 0, 
+    scrollTrigger: {
+        trigger: '.hero-section',
+        start: 'center',
+        end: '820',
+        scrub: true
+    }
+})
+
+/*    LEFT    */ 
+
+let itemsL = gsap.utils.toArray('.gallery__left .gallery__items')
+
+itemsL.forEach(item => {
+    gsap.fromTo(item, {x: -50,opacity: 0},{
+        opacity: 1, x:0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-100',
+            scrub: true
+        }
+    })
+})
+
+/*    Right    */ 
+
+let itemsR = gsap.utils.toArray('.gallery__right .gallery__items')
+
+itemsR.forEach(item => {
+    gsap.fromTo(item, {x: 50,opacity: 0},{
+        opacity: 1, x:0,
+        scrollTrigger: {
+            trigger: item,
+            start: '-850',
+            end: '-100',
+            scrub: true
+        }
+    })
+})
